047d676e6518a61919fd51dc4d9b9fac04d4f5fb
alpsakaci <alp@alpsakaci.com>
2026-01-02 21:02:51 +0300

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyline-core",
-    version="2.0.0",
+    version="2.1.0",
     packages=find_packages(),
     author="Alp Sakaci",
     author_email="alp@alpsakaci.com",
